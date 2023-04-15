3af39d42fc4a9f7fc3a6b1fa9abc019187c6b059
Sofyan Sitorus <sofyansitorus@gmail.com>
2023-04-15 06:03:00 +0000

    first commit

---
+++ loker-api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Loker API
+ * Plugin URI:      https://github.com/sofyansitorus/loker-api
+ * Description:     REST API Server for Loker
+ * Author:          Sofyan Sitorus
+ * Author URI:      https://github.com/sofyansitorus
+ * Text Domain:     loker-api
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Loker_Api
+ */
+
+// Your code starts here.
